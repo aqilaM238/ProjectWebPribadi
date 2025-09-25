8b25694578e2e607b6e70cafbe037747e40b0c4d
aqilaM238 <aqilamuhammadasqolan@gmail.com>
2025-09-25 20:09:27 +0700

    init

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+Route::get('/berita', function () {
+    return view('berita');
+});
+
+Route::get('/kontak', function () {
+    return view('kontak');
+});
